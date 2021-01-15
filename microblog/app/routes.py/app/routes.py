acc342f368eac8dcdf21b033f994c6586384fac8
nsivakumar03 <you@example.com>
2021-01-15 08:40:13 -0600

    Finished ch2 and 3 and halfway with 4

--- microblog/app/routes.py/app/routes.py
+++ microblog/app/routes.py/app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
 @app.route('/')
 @app.route('/index')
@@ -23,3 +23,12 @@ def index():
         <h1>Hello''' + user['username'] + '''!</h1>
     </body>
 </html>
+
+@app.route('/login', methods= ['GET','POST'])
+def login():
+    form = LoginForm()
+    if form.validates_on_submit():
+        flash('Login requested for user (), remember_me=()' .format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('/index'))
+    return render_template('login.html', title= 'Sign In', form=form)
