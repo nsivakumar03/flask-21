d1bdc98602ce762ffaa670d511862a64399623cd
nsivakumar03 <you@example.com>
2021-01-18 22:28:00 -0600

    Mega flask tutorial done. Might need debugging.

--- microblog/app/models.py
+++ microblog/app/models.py
@@ -17,6 +17,29 @@ class User(UserMixin, db.Model):
         self.password_hash = generate_password_hash(password)
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    followed = db.relationship(
+        'User', secondary=followers,
+        primaryjoin=(followers.c.follower_id == id),
+        secondaryjoin=(followers.c.followed_id == id),
+        backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')
+
+    def follow(self, user):
+        if not self.is_following(user):
+            self.followed.append(user)
+    def unfollow(self, user):
+        if self.is_following(user):
+            self.followed.remove(user)
+    def is_following(self, user):
+        return self.followed.filter(
+            followers.c.followed_id == user.id).count() > 0
+
+    def followed_posts(self):
+        return Post.query.join(
+            followers, (followers.c.followed_id == Post.user_id)).filter(
+                followers.c.follower_id == self.id).order_by(
+                    Post.timestamp.desc())
+                
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String(140))
@@ -29,3 +52,15 @@ class Post(db.Model):
 @login_user_loader
 def load_user(id)
     return Uer.query.get(int(id))
+
+followers = db.Table('followers',
+    db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
+)
+
+def followed_posts(self):
+    followed = Post.query.join(
+        followers, (followers.c.followed_id == Post.user_id)).filter(
+            followers.c.follower_id == self.id)
+    own = Post.query.filter_by(user_id=self.id)
+    return followed.union(own).order_by(Post.timestamp.desc())
